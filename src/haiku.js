3d9ad86d729abaa2263b005d520a4eca49f002ca
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-04-15 11:42:36 -0700

    add business logic to pass a test

--- src/haiku.js
+++ src/haiku.js
@@ -1,6 +1,18 @@
 export class Haiku {
   constructor (poem) {
     this.poem = poem;
+    this.lines = [];
+  }
 
+  lineCount() {
+   
+    const splitLines = this.poem.split("\n");
+    splitLines.forEach(line => {
+      this.lines.push(line);
+     });
+    
+     if (this.lines.length != 3) {
+       return false;
+     }
   }
 }
