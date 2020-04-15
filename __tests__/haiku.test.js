15ec61e6a25be1b3cd5ca1b148e6eb1865139d36
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-04-15 11:38:36 -0700

    implement meaningful fail test lineCount, succesful fail

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -2,12 +2,17 @@ import { Haiku } from './../src/haiku.js';
 
 describe('Haiku', () => { 
   let poem;
-
+  
   it('should correctly create a haiku object containing one string', () => {
-    var haiku = new Haiku("On an island of music in a city of drumbeats the drum dream girldreamed");
+    let haiku = new Haiku("On an island of music in a city of drumbeats the drum dream girldreamed");
     expect(typeof haiku.poem).toBe('string');
   });
 
+  test('should check to see if poem has three lines', () => {
+    let poem = new Haiku("On an island of music \n in a city of drumbeats \n the drum dream girldreamed");
+    poem.lineCount();
+    expect(poem.lines.length).toBe(3);
+  });
 
   
 })
