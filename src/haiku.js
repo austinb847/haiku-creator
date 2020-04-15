8dbc83474fd47a747159f83ca6b91794e889fd65
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-04-15 15:52:31 -0700

    add business logic and pass test for wordSplit for line 1

--- src/haiku.js
+++ src/haiku.js
@@ -2,6 +2,9 @@ export class Haiku {
   constructor (poem) {
     this.poem = poem;
     this.lines = [];
+    this.lineOne = [];
+    this.lineTwo = [];
+    this.lineThree = [];
   }
 
   lineCount() {
@@ -14,6 +17,11 @@ export class Haiku {
   lineCheck() {
     if (this.lines.length != 3) {
       return false;
-   }
+    }
+  }
+
+  wordSplit(line) {
+    const splitWords = line.split(" ");
+    return splitWords.filter(function(entry) { return entry.trim() != ''; });
   }
 }
